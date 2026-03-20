cc9805d0df64d0b656be1c525541641c4668bf85
lemon07r <lemon07r@gmail.com>
2026-03-20 15:43:29 -0400

    chore(release): v4.1.6

--- scripts/ui/agent-browser-harness.tsx
+++ scripts/ui/agent-browser-harness.tsx
@@ -239,7 +239,7 @@ function Harness() {
             onAutoModeArm={() => {}}
             onAutoModeStop={() => {}}
             onAutoModePolicyChange={() => {}}
-            version="4.1.5"
+            version="4.1.6"
           />
         </div>
       </div>
